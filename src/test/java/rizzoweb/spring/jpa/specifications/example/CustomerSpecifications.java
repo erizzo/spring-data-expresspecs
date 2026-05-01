90699664a72bf2f58d680486a463d4e3fade5488
Eric Rizzo <github@rizzoweb.com>
2026-05-01 15:03:17 -0400

    Use more static imports in test/example code.

--- src/test/java/rizzoweb/spring/jpa/specifications/example/CustomerSpecifications.java
+++ src/test/java/rizzoweb/spring/jpa/specifications/example/CustomerSpecifications.java
@@ -1,8 +1,14 @@
 package rizzoweb.spring.jpa.specifications.example;
 
 
+import static rizzoweb.spring.jpa.specifications.BasicSpecifications.*;
+import static rizzoweb.spring.jpa.specifications.CollectionSpecifications.isNotEmpty;
+import static rizzoweb.spring.jpa.specifications.CollectionSpecifications.sizeAtLeast;
 import static rizzoweb.spring.jpa.specifications.RangeSpecifications.atLeast;
+import static rizzoweb.spring.jpa.specifications.RangeSpecifications.greaterThan;
 import static rizzoweb.spring.jpa.specifications.SpecificationExtensions.smartDistinct;
+import static rizzoweb.spring.jpa.specifications.StringSpecifications.contains;
+import static rizzoweb.spring.jpa.specifications.StringSpecifications.containsIgnoreCase;
 
 import java.time.LocalDate;
 import java.util.Collection;
@@ -10,49 +16,45 @@ import java.util.Collection;
 import org.jspecify.annotations.NonNull;
 import org.springframework.data.jpa.domain.Specification;
 
-import rizzoweb.spring.jpa.specifications.BasicSpecifications;
-import rizzoweb.spring.jpa.specifications.CollectionSpecifications;
-import rizzoweb.spring.jpa.specifications.RangeSpecifications;
-import rizzoweb.spring.jpa.specifications.StringSpecifications;
 import rizzoweb.spring.jpa.specifications.PropertyPath;
 
 public interface CustomerSpecifications {
 
 	public static @NonNull Specification<Customer> nameIs(String name) {
-		return BasicSpecifications.is(Customer.Fields.name, name);
+		return is(Customer.Fields.name, name);
 	}
 
 	public static @NonNull Specification<Customer> nameIsOneOf(Collection<String> names) {
-		return BasicSpecifications.isAny(Customer.Fields.name, names);
+		return isAny(Customer.Fields.name, names);
 	}
 
 	public static @NonNull Specification<Customer> nameContains(String partialName) {
-		return StringSpecifications.contains(Customer.Fields.name, partialName);
+		return contains(Customer.Fields.name, partialName);
 	}
 
 	public static @NonNull Specification<Customer> nameContainsIgnoreCase(String partialName) {
-		return StringSpecifications.containsIgnoreCase(Customer.Fields.name, partialName);
+		return containsIgnoreCase(Customer.Fields.name, partialName);
 	}
 
 	public static @NonNull Specification<Customer> isActive() {
-		return BasicSpecifications.isTrue(Customer.Fields.isActive);
+		return isTrue(Customer.Fields.isActive);
 	}
 
 	public static @NonNull Specification<Customer> isNotActive() {
-		return BasicSpecifications.isFalse(Customer.Fields.isActive);
+		return isFalse(Customer.Fields.isActive);
 	}
 
 	public static @NonNull Specification<Customer> isActive(boolean value) {
-		return BasicSpecifications.is(Customer.Fields.isActive, value);
+		return is(Customer.Fields.isActive, value);
 	}
 
 	public static @NonNull Specification<Customer> hasZipCode(String zipCode) {
 		final var path = PropertyPath.of(Customer.Fields.address, Address.Fields.zipCode);
-		return BasicSpecifications.is(path, zipCode);
+		return is(path, zipCode);
 	}
 
 	public static @NonNull Specification<Customer> creditLimitOver(Integer minimum) {
-		return RangeSpecifications.greaterThan(Customer.Fields.creditLimit, minimum);
+		return greaterThan(Customer.Fields.creditLimit, minimum);
 	}
 
 	public static @NonNull Specification<Customer> hasRecentOrder() {
@@ -62,10 +64,10 @@ public interface CustomerSpecifications {
 	}
 
 	public static @NonNull Specification<Customer> hasAnyOrders() {
-		return CollectionSpecifications.isNotEmpty(Customer.Fields.orders);
+		return isNotEmpty(Customer.Fields.orders);
 	}
 
 	public static @NonNull Specification<Customer> hasAtLeastOrders(int minOrders) {
-		return CollectionSpecifications.sizeAtLeast(Customer.Fields.orders, minOrders);
+		return sizeAtLeast(Customer.Fields.orders, minOrders);
 	}
 }
